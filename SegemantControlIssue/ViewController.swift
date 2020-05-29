540dc1b8856f30b0e3d15d36aa2c8cd254b5ce95
orihpt <ori9070.hpt@gmail.com>
2020-05-29 14:04:43 +0300

    Source Code

---
+++ SegemantControlIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegemantControlIssue
+//
+//  Created by אורי האופטמן on 29/05/2020.
+//  Copyright © 2020 אורי האופטמן. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
